d152f5466875670057885cef19e20b14f6d57fa2
= <=>
2025-07-14 14:24:34 +0530

    Added Pagination and Average FrontEnd

--- src/main/java/com/example/urlshortener/repository/UrlRepository.java
+++ src/main/java/com/example/urlshortener/repository/UrlRepository.java
@@ -1,6 +1,8 @@
 package com.example.urlshortener.repository;
 
 import com.example.urlshortener.entity.Url;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,4 +10,6 @@ import java.util.Optional;
 public interface UrlRepository extends JpaRepository<Url, Long> {
     Optional<Url> findByShortUrl(String shortUrl);
     Optional<Url> findByOriginalUrl(String originalUrl);
+    Page<Url> findByUserEmail(String email, Pageable pageable);
+
 }
