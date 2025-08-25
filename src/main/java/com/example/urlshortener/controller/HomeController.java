2aebb90969f32c751f83b8949d0be58eb2f71662
gowthamreddy <gowthamreddysomala@gmail.com>
2025-08-25 07:25:43 +0530

    V1.0 Dockerfile and Dockerized

--- src/main/java/com/example/urlshortener/controller/HomeController.java
+++ src/main/java/com/example/urlshortener/controller/HomeController.java
@@ -11,4 +11,8 @@ public class HomeController {
         return new RedirectView("/index.html");
     }
 
+    @GetMapping("/active")
+    public String active() {
+        return "this is for Cronjob, current time in server is : " + System.currentTimeMillis();
+    }
 }
